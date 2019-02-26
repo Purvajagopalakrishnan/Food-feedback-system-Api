71ff9f72f332ac6bf994d0f46e632086eb76c832
purvaja gopalakrishnan <purvajag@CESCHN.COM>
2019-02-26 22:09:09 +0530

    Added controller without unit test

---
+++ Foodfeedback/Controllers/LoginController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Foodfeedback.DTO;
+using Foodfeedback.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Foodfeedback.Controllers
+{
+    [Route("api/login")]
+    public class LoginController: Controller
+    {
+        [HttpPost]
+        public bool GetUserdetails([FromBody] UserDTO user)
+        {
+            var entities = new FoodfeedbackDBContext();
+            var result = entities.Users.Where(x => x.Email == user.Email && x.Password == user.Password).Any();
+            return result;
+        }
+        public HttpResponseMessage Options()
+        {
+            var response = new HttpResponseMessage();
+            response.StatusCode = HttpStatusCode.OK;
+            return response;
+        }
+    }
+}
---
+++ Foodfeedback/Models/Users.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Foodfeedback.Models
+{
+    public partial class Users
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string EmpId { get; set; }
+    }
+}
